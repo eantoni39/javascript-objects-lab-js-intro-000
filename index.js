438975525e3b18626465f5c25025e25df063e398
emmanuel antoni <emmanuel.antoni@blackrock.com>
2019-03-15 23:50:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ function updateObjectWithKeyAndValue (object,key,value) {
 function updateObjectWithKeyAndValue (object,key,value) {
   return Object.assign({},object,{[key]:value})
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
+  object[key]=value
+  return value
+}
