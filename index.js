20f9a3ddf05041345c3bb6fbaf4947f3a72a2221
emmanuel antoni <emmanuel.antoni@blackrock.com>
2019-03-15 23:46:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue (object,key,value) {
-  return Object.assign(object,{key:'value'})
+  return Object.assign(object,{[key]:value})
 }
 
 function updateObjectWithKeyAndValue (object,key,value) {
-  return Object.assign({},object,{key:'value'})
+  return Object.assign({},object,{[key]:value})
 }
+
