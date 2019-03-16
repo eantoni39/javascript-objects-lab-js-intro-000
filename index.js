dcf54cb18aa29e8ed058b5665f0e90a1eb74b60a
emmanuel antoni <emmanuel.antoni@blackrock.com>
2019-03-16 00:05:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,3 +23,8 @@ function deleteFromObjectByKey(object,key) {
   delete newObj[key]
   return object,newObj
 }
+
+function destructivelyDeleteFromObjectByKey (object,key) {
+  delete object[key]
+  return object
+}
